704e45c8b522b0aabffa0bdaa8951bd6e656a8cd
Mahfuzul Haque <mafhaque@darenasolutions.com>
2019-10-12 21:35:25 -0400

    started work on integrating google identity

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { GoogleLogin } from 'react-google-login';
 
 function App() {
+  const response = user => {
+    console.log(user)
+  }
+
   return (
     <div>
+      <GoogleLogin
+        clientId={process.env.REACT_APP_CLIENT_ID}
+        render={() => <div></div>}
+        onSuccess={response}
+        onFailure={response}
+        cookiePolicy={'single_host_origin'}
+        autoLoad
+      />
     </div>
   );
 }
